584d16a87d4c5f6a9c0940372442c547aa9f3c97
Stasya <ap1050167@gmail.com>
2025-05-13 11:15:49 +0400

    added with konspekty

---
+++ konspekty/decorator.py
@@ -0,0 +1,49 @@
+import inspect
+import asyncio
+from datetime import datetime
+from functools import wraps
+
+
+def decorator(count):
+    def wrapper(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            start = datetime.now()
+            for _ in range(count):
+                res = func(*args, **kwargs)
+            time_res = datetime.now() - start
+            print(f"func name: {func.__name__}, time: {time_res}")
+            return res
+
+        @wraps(func)
+        async def async_wrapped(*args, **kwargs):
+            start = datetime.now()
+            for _ in range(count):
+                res = await func(*args, **kwargs)
+            time_res = datetime.now() - start
+            print(f"func name: {func.__name__}, time: {time_res}")
+            return res
+
+        if inspect.iscoroutinefunction(func):
+            return async_wrapped
+        else:
+            return wrapped
+
+    return wrapper
+
+
+@decorator(count=3)
+def foo(x, y):
+    return x ** y
+
+
+@decorator(count=3)
+async def async_foo(x, y):
+    return x ** y
+
+
+print(foo.__name__)
+print(foo(5, 5))
+
+print(async_foo.__name__)
+print(asyncio.run(async_foo(5, 5)))
